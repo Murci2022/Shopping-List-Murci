e6bf76c94e072f34e958ceec7a90cac60c16e936
Mate Kamaras <103876505+Murci2022@users.noreply.github.com>
2022-09-05 23:27:35 +0200

    add useEffect, async function API to App.jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const endpoint = "https://fetch-me.vercel.app/api/shopping/items";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(endpoint);
+      const json = await response.json();
+      setData(json.data);
+    }
+    fetchData();
+    console.log(data);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
     </div>
   );
 }
